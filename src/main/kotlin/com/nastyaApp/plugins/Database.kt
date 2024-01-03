711f15cb09f43b172a5a3861169424047be34911
Julia1999000 <jyliu1999@gmail.com>
2024-01-03 17:45:34 +0300

    Setting up Environmental Variables. creating tables

--- src/main/kotlin/com/nastyaApp/plugins/Database.kt
+++ src/main/kotlin/com/nastyaApp/plugins/Database.kt
@@ -1,19 +1,38 @@
 package com.nastyaApp.plugins
 
+import com.nastyaApp.controllers.*
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
-    val database = Database.connect("jdbc:postgresql://localhost:5432/nastyaapp", // TODO nameDB
+    val database = Database.connect(
+        url = System.getenv("DB_URL"),
         driver = "org.postgresql.Driver",
-        user = "postgres",
-        password = "2514" // TODO password
+        user = System.getenv("DB_USER"),
+        password = System.getenv("DB_PASSWORD")
     )
 
     transaction {
         addLogger(StdOutSqlLogger)
+
+        SchemaUtils.create(
+            FilesController.FileTypesTable,
+            FilesController.ImagesTable,
+            UsersController.UsersTable,
+            UserTokensController.UserTokensTable,
+            AdminsController.AdminsTable,
+            AdminTokensController.AdminTokensTable,
+            ComsController.ComStatusesTable,
+            ComsController.ComsTable,
+            CommentController.CommentsTable,
+            LikesController.LikersToComsTable,
+            BoardController.BoardStatusesTable,
+            BoardController.BoardsTable,
+            BoardController.BoardsToComsTable
+        )
     }
 }
