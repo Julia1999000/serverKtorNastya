d4b21f816894ecce3b43b63bb9a6d393705693aa
Julia1999000 <jyliu1999@gmail.com>
2024-01-05 19:57:33 +0300

    created environment variables of the server port and host to generate a link to the images

--- src/main/kotlin/com/nastyaApp/Application.kt
+++ src/main/kotlin/com/nastyaApp/Application.kt
@@ -6,8 +6,12 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8090, host = "0.0.0.0", module = Application::module)
-            .start(wait = true)
+    embeddedServer(
+        Netty,
+        port = System.getenv("SERVER_PORT").toInt(),
+        host = System.getenv("SERVER_HOST"),
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
--- src/main/kotlin/com/nastyaApp/utils/UrlUtilities.kt
+++ src/main/kotlin/com/nastyaApp/utils/UrlUtilities.kt
@@ -5,6 +5,6 @@ import java.util.UUID
 
 fun generateImagUrl(id: UUID?): String? {
     return id?.let {
-        "http://127.0.0.1:8080/file/images/open/${it}"
+        "http://${System.getenv("SERVER_HOST")}:${System.getenv("SERVER_PORT")}/file/images/open/${it}"
     }
 }
